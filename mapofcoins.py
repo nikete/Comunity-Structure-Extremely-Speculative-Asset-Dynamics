7cbc878e1966d8e1666cd5893ea69bdbfebc8ba4
Octavio Carlos Bunge Campos <octabunge@gmail.com>
2015-10-16 22:11:41 -0300

    added/updated scrapes from mapofcoins

--- mapofcoins.py
+++ mapofcoins.py
@@ -1,8 +1,16 @@
 import json, urllib2, csv
 from bs4 import BeautifulSoup
 
-with open('mapofcoins.json') as jsonfile:
-	coins = json.load(jsonfile)
+with open('mapofcoinsbtc.json') as jsonfile:
+	btccoins = json.load(jsonfile)
+with open('mapofcoinsnxt.json') as jsonfile:
+	ripplecoins = json.load(jsonfile)
+with open('mapofcoinsripple.json') as jsonfile:
+	nxtcoins = json.load(jsonfile)
+with open('mapofcoinsbytecoin.json') as jsonfile:
+	bytecoins = json.load(jsonfile)
+
+
 
 def coin_travel(coin, coinlist):
 	if 'children' in coin.keys():
@@ -13,8 +21,12 @@ def coin_travel(coin, coinlist):
 		coinlist.append(coin)
 
 coinlist = []
-coin_travel(coins['coins'], coinlist)
+coin_travel(btccoins['coins'], coinlist)
+coin_travel(ripplecoins['coins'], coinlist)
+coin_travel(nxtcoins['coins'], coinlist)
+coin_travel(bytecoins['coins'], coinlist)
 processed_list = []
+processed_list_ann = []
 for coin in coinlist:
 	if coin['PoS'] == 'no':
 		pos = False
@@ -24,32 +36,46 @@ for coin in coinlist:
 		premine = False
 	else:
 		premine = True
-	pow_ = coin['PoW']
+	if 'PoW' in coin.keys():
+		pow_ = coin['PoW']
+	else:
+		pow_ = ''
 	technical = False
 	name = coin['name']
 	if 'children' in coin.keys():
 		children_amount = len(coin['children'])
 	else:
 		children_amount = 0
-	algorithm = coin['algo']
+	if 'algo' in coin.keys():
+		algorithm = coin['algo']
+	else:
+		algo = ''
 	if 'ForkedFrom' in coin.keys():
 		if coin['ForkedFrom'] != '':
 			for c in coinlist:
 				if coin['ForkedFrom'] == c['name']:
-					if c['algo'] != coin['algo']:
-						technical = True
+					if 'algo' in c.keys() and 'algo' in coin.keys():
+						if c['algo'] != coin['algo']:
+							technical = True
+					elif 'algo' in c.keys() or 'algo'in coin.keys():
+							technical = True
+		else:
+			technichal = True
+	if 'ANN'in coin.keys():
+		ann = coin['ANN']
+	else:
+		ann = ''
 	processed_list.append([str(name), str(children_amount), str(algorithm), str(technical), str(premine), str(pos)])		
+	processed_list_ann.append([str(name), str(ann)])
 #	processed_list.append({'symbol': name, 'children_amount': children_amount, 'algorithm': algorithm, 'technical': technical, 'premine':premine, 'pos':pos, 'pow':pow_})
-fieldnames = [
-'symbol',
-'children_amount',
-'algorithm',
-'technical',
-'premine',
-'pos',
-'pow'
-]
+
+
 with open('technicality.csv','wb') as csvfile:
 	csvfile.write(str(','.join(['name', 'children_amount', 'algorithm', 'technical', 'premine', 'pos'])+'\n'))
 	for coin in processed_list:
 		csvfile.write(str(', '.join(coin)+'\n'))
+
+with open('ann.csv','wb')as csvfile:
+	csvfile.write(str(','.join(['symbol','ann'])+'\n'))
+	for coin in processed_list_ann:
+		csvfile.write(str(','.join(coin)+'\n'))
