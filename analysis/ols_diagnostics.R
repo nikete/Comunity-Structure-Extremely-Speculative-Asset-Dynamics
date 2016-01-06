39ce002aca3038ee0d44aff9925e3c30b2979d11
Eaman Jahani <eamanj@gmail.com>
2016-01-06 14:27:49 -0500

    update ols diagnostics with new data

--- analysis/ols_diagnostics.R
+++ analysis/ols_diagnostics.R
@@ -1,12 +1,18 @@
 library(car)
 library(lmtest)
 setwd(dir = "~/research/nikete/Comunity-Structure-Extremely-Speculative-Asset-Dynamics/data")
-data = read.csv(file = "joined_with_fallback.csv", header = TRUE, sep = ",")
-data = read.csv(file = "joined_with_fallback_d200.csv", header = TRUE, sep = ",")
-data = read.csv(file = "joined_with_fallback_d100.csv", header = TRUE, sep = ",")
+data = read.csv(file = "joined_price_network_usd.csv", header = TRUE, sep = ",")
 data$network_date = as.character(data$network_date, format = "%Y-%m-%d")
 data$earliest_trade_date = as.character(data$earliest_trade_date, format = "%Y-%m-%d")
+data$earliest_mention_date = as.character(data$earliest_mention_date, format = "%Y-%m-%d")
 data$log_severity_to_average_after_max_volume_weighted = log(data$severity_to_average_after_max_volume_weighted)
+data$magnitude = data$normalized_total_volume_before_max / data$normalized_total_volume
+# fix infinite satoshi distance
+data$user1_satoshi_distance_inf = data$user1_satoshi_distance>7
+data$user1_satoshi_distance[data$user1_satoshi_distance_inf] = 7
+
+# remove btc if present
+data = data[data$symbol != "BTC",]
 
 data_size = nrow(data)
 # 60% train
@@ -18,30 +24,21 @@ train_indices = sample(seq(data_size), size = train_size)
 train_data = data[train_indices, ]
 test_data = data[-train_indices, ]
 
-lmfit = lm(log_severity_to_average_after_max_volume_weighted ~
-           #user1_num_mentions +
-           user1_num_posts +
-           user1_num_subjects +
-           user1_days_since_first_post +
-           user1_degree_total +
-           user1_degree_incoming +
-           #user1_degree_outgoing +
-           user1_clustering_coefficient +
-           #user1_closeness_centrality_unweighted + 
-           user1_closeness_centrality_weighted + 
-           #user1_closeness_centrality_incoming_unweighted + 
-           #user1_closeness_centrality_outgoing_unweighted + 
-           #user1_closeness_centrality_incoming_weighted + 
-           #user1_closeness_centrality_outgoing_weighted +
-           user1_betweenness_centrality_weighted +
-           #user1_satoshi_distance +
-           #user1_satoshi_pagerank_unweighted +
-           user1_satoshi_pagerank_weighted +
-           #user1_pagerank_unweighted +
-           user1_pagerank_weighted +
-           nontrivial +
-           nontrivial*user1_closeness_centrality_weighted,
-           train_data)
+independent_vars =  c("user1_num_posts",
+                      "user1_num_subjects",
+                      "user1_days_since_first_post",
+                      "user1_degree_incoming",
+                      "user1_degree_outgoing",
+                      "user1_clustering_coefficient",
+                      "user1_closeness_centrality_weighted",
+                      "user1_betweenness_centrality_weighted",
+                      "user1_satoshi_distance",
+                      "user1_satoshi_distance_inf",
+                      "user1_satoshi_pagerank_weighted",
+                      "user1_pagerank_weighted")
+dependent_var = "log_severity_to_average_after_max_volume_weighted"
+lm_formula = paste(dependent_var, "~", paste(independent_vars, collapse=" + "))
+lmfit = lm(as.formula(lm_formula), train_data)
 summary(lmfit)
 plot(lmfit, which=1, ask=F)
 plot(lmfit, which=2, ask=F)
@@ -49,30 +46,9 @@ plot(lmfit, which=3, ask=F)
 plot(lmfit, which=4, ask=F)
 plot(lmfit, which=5, ask=F)
 
-vars =  c("log_severity_to_average_after_max_volume_weighted",
-          "user1_num_posts",
-          "user1_num_subjects",
-          "user1_days_since_first_post",
-          "user1_degree_incoming",
-          "user1_degree_outgoing",
-          "user1_clustering_coefficient",
-          "user1_closeness_centrality_unweighted",
-          "user1_closeness_centrality_weighted",
-          "user1_closeness_centrality_incoming_unweighted",
-          "user1_closeness_centrality_outgoing_unweighted",
-          "user1_closeness_centrality_incoming_weighted",
-          "user1_closeness_centrality_outgoing_weighted",
-          "user1_betweenness_centrality_weighted",
-          "user1_satoshi_distance",
-          "user1_satoshi_pagerank_unweighted",
-          "user1_satoshi_pagerank_weighted",
-          "user1_pagerank_unweighted",
-          "user1_pagerank_weighted",
-          "nontrivial")
-
 # get outlier points
-train_data[which(cooks.distance(lmfit) > 50/nrow(train_data)), vars]
-which(cooks.distance(lmfit) > 50/nrow(train_data))
+train_data[which(cooks.distance(lmfit) > 30/nrow(train_data)), independent_vars]
+which(cooks.distance(lmfit) > 30/nrow(train_data))
 plot(cooks.distance(lmfit))
 influencePlot(lmfit)
 
@@ -80,11 +56,14 @@ influencePlot(lmfit)
 bptest(lmfit)
 
 # remove highly influential points
-remove = -c(44,115)
+remove = -c(144,335)
 train_data = train_data[remove,]
-remove = -c(135)
+remove = -c(14)
+train_data = train_data[remove,]
+remove = -c(121)
 train_data = train_data[remove,]
 
 # test
 predictions = predict.lm(lmfit, test_data)
-plot(predictions, test_data$severity_to_average_after_max_volume_weighted, xlim=c(0,5), ylim=c(0,5))
+plot(predictions, test_data[,dependent_var], xlim=c(0,10), ylim=c(0,10), ylab="actual", xlab="predicted")
+abline(a=0, b=1)
