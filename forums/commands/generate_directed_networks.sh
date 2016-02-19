1c0b10d3b913378e73e19aaddc8ab161b246e600
Eaman Jahani <eamanj@gmail.com>
2016-02-19 12:59:39 -0500

    fix generate network commands

--- forums/commands/generate_directed_networks.sh
+++ forums/commands/generate_directed_networks.sh
@@ -1,4 +1,4 @@
-INPUT_FILE="/home/eaman/research/bitcoin/nikete_tmp/forum-data/unmodified-symbol-or-name-in-subject----unmodified-symbol-or-name-in-subject-sorted-by-time.csv"
-OUTPUT_DIR="/home/eaman/research/bitcoin/nikete_tmp/networks"
+INPUT_FILE="./forum-data/unmodified-symbol-or-name-in-subject----unmodified-symbol-or-name-in-subject-sorted-by-time.csv"
+OUTPUT_DIR="./networks"
 
-../analysis/generate_networks.py $INPUT_FILE ${OUTPUT_DIR}/directed_unlimited -dn
+./forums/analysis/generate_networks.py $INPUT_FILE ${OUTPUT_DIR}/directed_unlimited -dn
